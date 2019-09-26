a9683c7f07d75fa66ddb3bb729c4d7de243858e9
kuworking <kuworking@gmail.com>
2019-09-26 08:59:17 +0200

    wip

--- config.js
+++ config.js
@@ -9,7 +9,29 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Mejores Escape Rooms de Barcelona!',
+  heading: 'Escape Rooms de Barcelona',
   subHeading:
     'Si quieres recibir cada semana información sobre los mejores Escape Rooms de Barcelona, inscríbete en nuestra lista de correo',
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
 };
